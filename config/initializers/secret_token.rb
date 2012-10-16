9fb6dbc7f5c8ea037f393eeb7ffedc88180428b1
Dan Buettner <drbuettner@gmail.com>
2012-10-15 19:29:24 -0500

    bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikileaks::Application.config.secret_token = '7b2d9ce4258907bb66e3cff0dfea98427bc6fa98d7b28a1e4564e7f7e2a19e9eb76173ffcf01fb3f31316a48e790253f7922961ca7a3b66be48ce5a05f039ab1'
