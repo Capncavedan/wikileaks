7302c21df4ebb6ad198da3570d858d83fc62b27b
Dan Buettner <drbuettner@gmail.com>
2012-10-16 10:43:55 -0500

    working fulltext search

--- app/controllers/leaks_controller.rb
+++ app/controllers/leaks_controller.rb
@@ -1,9 +1,20 @@
 class LeaksController < ApplicationController
 
   def index
+    do_search
   end
 
   def show
+  end
+
+  private
+
+  def do_search
+    @hits = []
+    return if params[:q].blank?
+    @hits = Leak.search do
+      fulltext params[:q]
+    end.hits
   end
 
 end
