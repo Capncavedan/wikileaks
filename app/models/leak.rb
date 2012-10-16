d1083c80a1559dcc70c476b648c6527482bde173
Dan Buettner <drbuettner@gmail.com>
2012-10-16 00:32:53 -0500

    additional searchable fields

--- app/models/leak.rb
+++ app/models/leak.rb
@@ -1,11 +1,19 @@
 class Leak < ActiveRecord::Base
 
   searchable do
+    text :header
     text :body
+    text :origin_id
+    text :destination_id
+
+    string :classification
+    string :origin_description
+
+    time :cable_date
   end
 
 
-  def self.search(user_search_string)
+  def self.sql_search(user_search_string)
     conditions = []
     user_search_string.split(' ').each do |term|
       conditions << sanitize_sql_array(["body LIKE ?", "%#{term}%"])
